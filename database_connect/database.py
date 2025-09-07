620a937303c53119facb836e4ece1e96b0c5dfeb
arnab123dotcom <69048126+arnab123dotcom@users.noreply.github.com>
2025-09-07 13:20:08 +0530

    Update database.py

--- database_connect/database.py
+++ database_connect/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-database_url = "postgresql://postgres:arnab123#@localhost:5433/librarysys"
+database_url = "postgresql://postgres:_______@localhost:5433/librarysys"
 
 engine = create_engine(database_url)
 sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -11,4 +11,5 @@ def get_db():
     try:
         yield db
     finally:
+
         db.close()
